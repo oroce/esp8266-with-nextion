095660e8a60afafd54752cdd7700e16eb937e006
oroce <robert@oroszi.net>
2017-12-28 12:02:06 +0100

    feat: remove nextion/request-state message

--- src/main.ino
+++ src/main.ino
@@ -114,7 +114,6 @@ void reconnect()
             //mqttClient.publish(statusTopic, "online");
             // ... and resubscribe
             state.isMQTTConnected = true;
-            mqttClient.publish("nextion/request-state", "on");
             boolean subscribed = mqttClient.subscribe(listenToTopic);
             if (subscribed) {
                 Serial.println("subscribed");
